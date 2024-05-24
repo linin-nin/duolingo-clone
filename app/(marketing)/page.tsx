ae34ffeea329dbeca9c551901f120be8346967b9
nin <snin934@gmail.com>
2024-05-24 21:40:54 +0700

    first commit

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-4 space-y-4 flex flex-col max-w-[200px]">
+      mmmmm
+    </div>
+  );
+}
